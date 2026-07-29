37d1c73421ec51aa761c95be2b763e340c078ebb
Enrico Barni Venturato <enricobarni28@gmail.com>
2026-07-29 20:22:43 -0300

    feat: Adicionando builder do DB ao Program.cs

--- src/TechDebtHub.Api/Program.cs
+++ src/TechDebtHub.Api/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using TechDebtHub.Infrastructure;
 using TechDebtHub.Infrastructure.Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,12 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-{
-    var connectionString = builder.Configuration.GetConnectionString("SqlServer");
-
-    options.UseSqlServer(connectionString);
-});
+builder.Services.AddInfrastructure(builder.Configuration);
 
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
