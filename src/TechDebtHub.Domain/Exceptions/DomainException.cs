940343fdad560da62361963c6f37f621bbabdbc7
Enrico Barni Venturato <enricobarni28@gmail.com>
2026-07-30 15:34:42 -0300

    feat: Criando arquivos de exceptions na camada Domain

---
+++ src/TechDebtHub.Domain/Exceptions/DomainException.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TechDebtHub.Domain.Exceptions
+{
+    public sealed class DomainException : Exception
+    {
+        public DomainException(string message)
+            : base(message) { }
+    }
+}
