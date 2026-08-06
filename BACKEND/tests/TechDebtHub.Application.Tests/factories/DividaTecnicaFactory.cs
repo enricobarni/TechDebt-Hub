abc0aa3038173604febc027bc441caf68b1d231a
Enrico Barni Venturato <enricobarni28@gmail.com>
2026-08-06 16:06:36 -0300

    feat: adicionando fabrica de teste para DividaTecnica

---
+++ BACKEND/tests/TechDebtHub.Application.Tests/factories/DividaTecnicaFactory.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TechDebtHub.Domain.Entities;
+using TechDebtHub.Domain.Enums;
+
+namespace TechDebtHub.Application.Tests.Factories
+{
+    public static class DividaTecnicaFactory
+    {
+        public static DividaTecnica Criar(
+            Guid projetoId,
+            string titulo = "Consulta sem paginação",
+            string descricao = "A consulta retorna todos os registros.",
+            CategoriaDivida categoria = CategoriaDivida.Performance,
+            NivelImpacto impacto = NivelImpacto.Alto,
+            NivelUrgencia urgencia = NivelUrgencia.Media,
+            NivelFrequencia frequencia = NivelFrequencia.Constante,
+            NivelEsforco esforco = NivelEsforco.Medio
+        )
+        {
+            return DividaTecnica.Criar(
+                projetoId,
+                titulo,
+                descricao,
+                categoria,
+                impacto,
+                urgencia,
+                frequencia,
+                esforco
+            );
+        }
+    }
+}
