11a7ce670c0094acc66c53d1e6684426bc26679c
enricobarni <enricobarni28@gmail.com>
2026-08-12 21:12:00 -0300

    feat: criando controller e adicionando primeiro endpoint para cadastrar usuario

---
+++ BACKEND/src/TechDebtHub.Api/Controllers/AuthController.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TechDebtHub.Api.Contracts.Usuarios;
+using TechDebtHub.Application.Features.Usuarios.CadastrarUsuario;
+
+namespace TechDebtHub.Api.Controllers
+{
+    [ApiController]
+    [Route("auth")]
+    public class AuthController : ControllerBase
+    {
+        private readonly CadastrarUsuarioHandler _cadastrarUsuarioHandler;
+
+        public AuthController(CadastrarUsuarioHandler cadastrarUsuarioHandler)
+        {
+            _cadastrarUsuarioHandler = cadastrarUsuarioHandler;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<CadastrarUsuarioResponse>> Cadastrar(
+            [FromBody] CadastrarUsuarioRequest request,
+            [FromServices] CadastrarUsuarioHandler handler,
+            CancellationToken cancellationToken
+        )
+        {
+            var command = new CadastrarUsuarioCommand(request.Nome, request.Email, request.Senha);
+
+            var response = await handler.HanldeAsync(command, cancellationToken);
+
+            return StatusCode(StatusCodes.Status201Created, response);
+        }
+    }
+}
