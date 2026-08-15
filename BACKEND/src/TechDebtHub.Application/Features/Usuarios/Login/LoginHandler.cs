573c5cd8639756c0e471fb0a46fd58384e8e0e97
enricobarni <enricobarni28@gmail.com>
2026-08-14 23:23:26 -0300

    update: atualizando LoginHandler para se adequar ao update do RefreshTokenGenerator

--- BACKEND/src/TechDebtHub.Application/Features/Usuarios/Login/LoginHandler.cs
+++ BACKEND/src/TechDebtHub.Application/Features/Usuarios/Login/LoginHandler.cs
@@ -6,6 +6,7 @@ using Microsoft.EntityFrameworkCore;
 using TechDebtHub.Application.Abstractions.Persistence;
 using TechDebtHub.Application.Exceptions;
 using TechDebtHub.Application.Interfaces;
+using TechDebtHub.Domain.Entities;
 
 namespace TechDebtHub.Application.Features.Usuarios.Login
 {
@@ -16,16 +17,22 @@ namespace TechDebtHub.Application.Features.Usuarios.Login
         private readonly IApplicationDbContext _context;
         private readonly IPasswordHasher _passwordHasher;
         private readonly IJwtTokenGenerator _jwtTokenGenerator;
+        private readonly IRefreshTokenGenerator _refreshTokenGenerator;
+        private readonly ITokenHasher _tokenHasher;
 
         public LoginHandler(
             IApplicationDbContext context,
             IPasswordHasher passwordHasher,
-            IJwtTokenGenerator jwtTokenGenerator
+            IJwtTokenGenerator jwtTokenGenerator,
+            IRefreshTokenGenerator refreshTokenGenerator,
+            ITokenHasher tokenHasher
         )
         {
             _context = context;
             _passwordHasher = passwordHasher;
             _jwtTokenGenerator = jwtTokenGenerator;
+            _refreshTokenGenerator = refreshTokenGenerator;
+            _tokenHasher = tokenHasher;
         }
 
         public async Task<LoginResponse> HandleAsync(
@@ -54,7 +61,26 @@ namespace TechDebtHub.Application.Features.Usuarios.Login
 
             var acessToken = _jwtTokenGenerator.Generate(usuario.Id);
 
-            return new LoginResponse(acessToken, "Bearer");
+            var (refreshTokenValue, refreshTokenExpiration) = _refreshTokenGenerator.Generate();
+
+            var refreshTokenHash = _tokenHasher.Hash(refreshTokenValue);
+
+            var refreshTokenEntity = new RefreshToken(
+                usuario.Id,
+                refreshTokenHash,
+                refreshTokenExpiration
+            );
+
+            _context.RefreshTokens.Add(refreshTokenEntity);
+
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return new LoginResponse(
+                acessToken,
+                refreshTokenValue,
+                "Bearer",
+                refreshTokenExpiration
+            );
         }
     }
 }
