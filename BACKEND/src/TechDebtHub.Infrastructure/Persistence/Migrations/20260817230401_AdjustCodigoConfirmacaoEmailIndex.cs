bcc759ba44dade4d7ef03593f1b54cc29874162e
enricobarni <enricobarni28@gmail.com>
2026-08-17 20:15:51 -0300

    refactor: ajustando índices na tabela TokensConfirmaçãoEmail

---
+++ BACKEND/src/TechDebtHub.Infrastructure/Persistence/Migrations/20260817230401_AdjustCodigoConfirmacaoEmailIndex.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TechDebtHub.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdjustCodigoConfirmacaoEmailIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_TokensConfirmaçãoEmail_CodigoHash",
+                table: "TokensConfirmaçãoEmail");
+
+            migrationBuilder.DropIndex(
+                name: "IX_TokensConfirmaçãoEmail_UsuarioId",
+                table: "TokensConfirmaçãoEmail");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TokensConfirmaçãoEmail_UsuarioId_CodigoHash",
+                table: "TokensConfirmaçãoEmail",
+                columns: new[] { "UsuarioId", "CodigoHash" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_TokensConfirmaçãoEmail_UsuarioId_CodigoHash",
+                table: "TokensConfirmaçãoEmail");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TokensConfirmaçãoEmail_CodigoHash",
+                table: "TokensConfirmaçãoEmail",
+                column: "CodigoHash",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TokensConfirmaçãoEmail_UsuarioId",
+                table: "TokensConfirmaçãoEmail",
+                column: "UsuarioId");
+        }
+    }
+}
