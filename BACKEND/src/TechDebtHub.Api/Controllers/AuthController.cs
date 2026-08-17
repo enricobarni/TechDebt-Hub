54acf07e7f17f4c7b10771be3898aafdb39ffcac
enricobarni <enricobarni28@gmail.com>
2026-08-17 10:34:17 -0300

    feat: adicionando endpoint para buscar informações do usuário atual

--- BACKEND/src/TechDebtHub.Api/Controllers/AuthController.cs
+++ BACKEND/src/TechDebtHub.Api/Controllers/AuthController.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using TechDebtHub.Api.Contracts.Auth;
 using TechDebtHub.Api.Contracts.Usuarios;
+using TechDebtHub.Application.Features.Usuarios.BuscarUsuarioAtual;
 using TechDebtHub.Application.Features.Usuarios.CadastrarUsuario;
 using TechDebtHub.Application.Features.Usuarios.Login;
 using TechDebtHub.Application.Features.Usuarios.RefreshTokens;
@@ -18,16 +20,19 @@ namespace TechDebtHub.Api.Controllers
         private readonly CadastrarUsuarioHandler _cadastrarUsuarioHandler;
         private readonly LoginHandler _loginHandler;
         private readonly RefreshTokensHandler _refreshTokensHandler;
+        private readonly BuscarUsuarioAtualHandler _buscarUsuarioAtualHandler;
 
         public AuthController(
             CadastrarUsuarioHandler cadastrarUsuarioHandler,
             LoginHandler loginHandler,
-            RefreshTokensHandler refreshTokensHandler
+            RefreshTokensHandler refreshTokensHandler,
+            BuscarUsuarioAtualHandler buscarUsuarioAtualHandler
         )
         {
             _cadastrarUsuarioHandler = cadastrarUsuarioHandler;
             _loginHandler = loginHandler;
             _refreshTokensHandler = refreshTokensHandler;
+            _buscarUsuarioAtualHandler = buscarUsuarioAtualHandler;
         }
 
         [HttpPost("register")]
@@ -65,6 +70,19 @@ namespace TechDebtHub.Api.Controllers
             var command = new RefreshTokensCommand(request.RefreshToken);
 
             var response = await _refreshTokensHandler.HandleAsync(command, cancellationToken);
+
+            return Ok(response);
+        }
+
+        [Authorize]
+        [HttpGet("me")]
+        public async Task<ActionResult<BuscarUsuarioAtualResponse>> Me(
+            CancellationToken cancellationToken
+        )
+        {
+            var query = new BuscarUsuarioAtualQuery();
+
+            var response = await _buscarUsuarioAtualHandler.HandleAsync(query, cancellationToken);
 
             return Ok(response);
         }
